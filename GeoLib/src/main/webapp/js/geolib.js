5ef8b775570f87d397d5147a84ee2a268fe70f03
marcgs <marc.gomez82@gmail.com>
2014-08-17 00:25:47 +0200

    Merged javascript files

---
+++ GeoLib/src/main/webapp/js/geolib.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    function loadGPXFileIntoGoogleMap(map, filename) {
+        $.ajax({url: filename,
+            dataType: "xml",
+            success: function (data) {
+                var parser = new GPXParser(data, map);
+                parser.setTrackColour("#ff0000");     // Set the track line colour
+                parser.setTrackWidth(5);          // Set the track line width
+                parser.setMinTrackPointDelta(0.001);      // Set the minimum distance between track points
+                parser.centerAndZoom(data);
+                parser.addTrackpointsToMap();         // Add the trackpoints
+                parser.addWaypointsToMap();           // Add the waypoints
+            }
+        });
+    }
+
+    $('[data-js-selector="fileupload"]').fileupload({
+        dataType: 'json',
+        done: function (e, data) {
+            var mapOptions = {
+                zoom: 8,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById("map"),
+                mapOptions);
+            loadGPXFileIntoGoogleMap(map, "/geolib/resources/geofiles/mostRecent");
+        }
+    });
+
+});
+
+
