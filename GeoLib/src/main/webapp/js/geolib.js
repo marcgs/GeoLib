5a87b0dbc2c75a3490f98ec79f86a0a4cfc5843f
marcgs <marc.gomez82@gmail.com>
2014-08-17 21:07:20 +0200

    Loading initial map

--- GeoLib/src/main/webapp/js/geolib.js
+++ GeoLib/src/main/webapp/js/geolib.js
@@ -1,5 +1,16 @@
 $(function () {
 
+    function initialize() {
+        var mapOptions = {
+            center: new google.maps.LatLng(41.4, 2.2),
+            zoom: 10
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
     function loadGPXFileIntoGoogleMap(map, filename) {
         $.ajax({url: filename,
             dataType: "xml",
@@ -15,6 +26,7 @@ $(function () {
         });
     }
 
+
     $('[data-js-selector="fileupload"]').fileupload({
         dataType: 'json',
         done: function (e, data) {
@@ -27,7 +39,6 @@ $(function () {
             loadGPXFileIntoGoogleMap(map, "/geolib/resources/geofiles/mostRecent");
         }
     });
-
 });
 
 
