f4e17beda7f78ed967c2b585f86906da1b5e0c4f
Marc Gomez <marc.gomez82@gmail.com>
2014-10-08 19:22:41 +0200

    Frontend build integrated in Maven

---
+++ karma.conf.ci.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var baseConfig = require('./karma.conf.js');
+
+module.exports = function(config){
+    // Load base config
+    baseConfig(config);
+
+    // Override base config
+    config.set({
+        singleRun: true,
+        autoWatch: false
+    });
+};
